649463400bde30fa2c12694d6da4ef2938d5c958
sophy8281 <wanjikusophia3@gmail.com>
2021-05-31 19:17:18 +0300

    first commit

---
+++ api/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+# Create your models here.
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    author = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s by %s' % (self.title, self.author)
---
+++ api/views.py
@@ -0,0 +1,14 @@
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework import generics
+# Create your views here.
+
+
+class BookList(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
